212aea9e35ac58ca222f89504246bc6e4f8c70e0
Augusto Nunes <augustocesar.udi@gmail.com>
2022-07-06 19:05:28 -0300

    Add Navbar

---
+++ frontweb/src/components/Navbar/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav className="main-header-container">
+        <h1 className="main-header-title">BDS vendas</h1>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
